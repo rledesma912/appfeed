56b2f55981747ee15afb2631fb4d43a5c3954ed1
unknown <hledesma@aftic39.cnc.inter>
2017-12-28 15:09:37 -0300

    github en la descripcion del video. Por ahora no arma el carousel. saluti.

---
+++ index.js
@@ -0,0 +1,55 @@
+
+const REMOTE = require('electron').remote
+const MAIN = REMOTE.require('./main.js')
+
+let button = document.createElement('button')
+
+button.textContent = "Open window"
+document.body.appendChild(button)
+
+button.addEventListener('click', () => {
+  MAIN.openWindow();
+})
+
+$(function(){
+  const OS = require('os')
+  const prettyBytes = require('pretty-bytes')
+  const UL = $('.flipster ul');
+
+  $('.stats').append('Número de procesadores:' + OS.cpus().length + '&nbsp;');
+  $('.stats').append('Mem libre:' + prettyBytes(OS.freemem()));
+
+	$.get('http://enupal.com/blog/rss', function(response){
+
+		const rss = $(response)
+
+		rss.find('item').each(function(){
+			const item = $(this)
+
+			const content = item.find('description').html().split('</a></div>')[0]+'</a></div>'
+
+			const urlRegex = /(http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?/g;
+
+			const imageSource = content.match(urlRegex)[1];
+
+			const li = $('<li><img /><a target="_blank"></a></li>')
+
+			li.find('a')
+				.attr('href', item.find('link').text())
+				.html('<br>'+item.find('title').text())
+
+			li.find('img').attr('src', imageSource)
+			li.find('img').attr('width', 400)
+			li.find('img').attr('height', 300)
+
+			li.appendTo(UL)
+		});
+
+		// init plugin
+		$('.flipster').flipster({
+			style: 'carousel'
+		});
+
+	});
+
+});
---
+++ main.js
@@ -0,0 +1,33 @@
+
+const ELECTRON = require("electron")
+const {app, BrowserWindow} = ELECTRON
+const PATH = require("path")
+const URL = require("url")
+
+let win
+
+function createWindow(){
+  win = new BrowserWindow({ height: 700, width: 800})
+
+  win.loadURL(URL.format({
+      pathname : PATH.join(__dirname, 'index.html'),
+      protocol : 'file',
+      slashes : true
+  }))
+
+  win.webContents.openDevTools()
+}
+
+exports.openWindow = () => {
+  let win2 = new BrowserWindow({ height: 800, width: 600})
+
+  win2.loadURL(URL.format({
+      pathname : PATH.join(__dirname, 'feeds.html'),
+      protocol : 'file',
+      slashes : true
+  }))
+
+
+}
+
+app.on('ready', createWindow)
